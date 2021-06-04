f0c61576ffbe6b2bb7741c82bbf647b06e7724c7
Jason Li <jasonli.aus@gmail.com>
2021-06-04 22:17:11 +1000

    04/06/2021 commit 2

---
+++ Database/SchoolProjectDatabase/Tables/Contact.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Contact]
+(
+	[COT_PK] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [COT_Title] NVARCHAR(10) NULL, 
+    [COT_FirstName] NVARCHAR(20) NOT NULL DEFAULT '', 
+    [COT_MiddleName] NVARCHAR(20) NULL, 
+    [COT_LastName] NVARCHAR(20) NOT NULL DEFAULT '', 
+    [COT_DateOfBirth] DATE NULL,
+    [COT_Address] NVARCHAR(150) NULL, 
+    [COT_PostCode] VARCHAR(10) NULL, 
+    [COT_Country] VARCHAR(20) NULL, 
+    [COT_Phone] VARCHAR(20) NULL, 
+    [COT_Email] VARCHAR(50) NULL, 
+    [COT_ContactByPhone] BIT NULL, 
+    [COT_ContactByEmail] BIT NULL, 
+    [COT_IsDeleted] BIT NOT NULL DEFAULT 0, 
+    [COT_IsActive] BIT NOT NULL DEFAULT 1, 
+)
